738736f4b34e8e715849ef0f70cb0194691d2629
Vishaal <nair@hotmail.ca>
2020-11-17 20:54:21 -0800

    Partially added java to courses

---
+++ misc/courselist.js
@@ -0,0 +1,7 @@
+
+var number = prompt("Enter a 4-digit code: ");
+let courseList = [
+    {code: "ACIT 1620", name: "Web Fundamental Technologies"},
+    {code: "ACIT 1420", name: "Intro to Systems Admin"},
+    {code: "MATH-1310", name: "Technical Math for IT"},
+]
